cb979d1d89afd1015dd761dd97a3b0b6a9d6ff13
Aayash <dangaayash@gmail.com>
2026-05-03 11:08:03 +0545

    User model done

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bycryptjs');
+
+const userSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: true, 
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        lowercase: true,
+        trim : true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        select: false
+    },
+    role: {
+        type: String,
+        enum: ['employee', 'admin'],
+        default: 'employee'
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+// Method to compare entered password with hashed password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  if (!candidatePassword || !this.password) return false;
+  return await bcrypt.compare(String(candidatePassword), this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
