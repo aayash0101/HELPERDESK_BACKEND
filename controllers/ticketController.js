071819a5193731966a1f76c458d5a6a86eb25047
Aayash <dangaayash@gmail.com>
2026-05-03 14:47:13 +0545

    Get ticket by Id

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -14,9 +14,21 @@ const createTicket = async (req, res) => {
 };
 
 const getMyTickets = async (req, res) => {
-    const tickets = await Ticket.find({ createdBy: req.user._id});
-    res.status(200).json({ success:true, tickets})
+    const tickets = await Ticket.find({ createdBy: req.user._id });
+    res.status(200).json({ success: true, tickets })
 };
 
-module.exports = {createTicket, getMyTickets}
+const getTicketById = async (req, res) => {
+    const ticket = await Ticket.findById(req.params.id);
+    if (!ticket) {
+       return res.status(404).json({ success: false, message: "No tickets found" });
+    }
+    if (ticket.createdBy.toString() !== req.user._id.toString()) {
+        res.status(403);
+        throw new Error('Not authorized to view this ticket');
+    }
+    res.status(200).json({ success: true, ticket });
+}
+
+module.exports = { createTicket, getMyTickets, getTicketById };
 
